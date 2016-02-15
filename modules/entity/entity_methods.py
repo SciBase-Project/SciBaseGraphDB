3e37c0b976512b244827d401ec9cf9db84d8df3c
cyberbeast <sandeshgade@gmail.com>
2016-02-15 16:09:48 +0530

    Final modular API Code Structure!

--- modules/entity/entity_methods.py
+++ modules/entity/entity_methods.py
@@ -4,3 +4,93 @@ import hug
 def create_journal_nodes(values):
 	# Code goes here!
 	return ()
+
+@hug.post('/create_article_nodes', versions=1)
+def create_article_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/attach_articles_to_journals', versions=1)
+def attach_articles_to_journals(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_journals', versions=1)
+def get_journals(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_journals_by_domain', versions=1)
+def get_journals_by_domain(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_journals_by_country', versions=1)
+def get_journals_by_country(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_journals_by_continent', versions=1)
+def get_journals_by_continent(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_journals_by_publisher', versions=1)
+def get_journals_by_publisher(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_articles_by_primary_author', versions=1)
+def get_articles_by_primary_author(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_articles_by_domain', versions=1)
+def get_articles_by_domain(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_articles_by_country', versions=1)
+def get_articles_by_country(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_articles_by_continent', versions=1)
+def get_articles_by_continent(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_articles_by_journal', versions=1)
+def get_articles_by_journal(values):
+	# Code goes here!
+	return ()
+
+@hug.delete('/delete_journal_nodes', versions=1)
+def delete_journal_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.delete('/delete_article_nodes', versions=1)
+def delete_article_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/update_journal_nodes', versions=1)
+def update_journal_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/update_article_nodes', versions=1)
+def update_article_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/create_domain', versions=1)
+def create_domain(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/attach_subdomain_to_domain', versions=1)
+def attach_subdomain_to_domain(values):
+	# Code goes here!
+	return ()
--- modules/member/member_methods.py
+++ modules/member/member_methods.py
@@ -4,3 +4,93 @@ import hug
 def create_author_nodes(values):
 	# Code goes here!
 	return ()
+
+@hug.get('/get_authors', versions=1)
+def get_authors(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_authors_by_affiliation', versions=1)
+def get_authors_by_affiliation(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_authors_by_origin_country', versions=1)
+def get_authors_by_origin_country(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_authors_by_resident_country', versions=1)
+def get_authors_by_resident_country(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_authors_by_domain', versions=1)
+def get_authors_by_domain(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/update_author_nodes', versions=1)
+def update_author_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.delete('/delete_author_nodes', versions=1)
+def delete_author_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/create_publisher_nodes', versions=1)
+def create_publisher_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_publishers', versions=1)
+def get_publishers(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_publishers_by_country', versions=1)
+def get_publishers_by_country(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_publishers_by_domain', versions=1)
+def get_publishers_by_domain(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/update_publisher_nodes', versions=1)
+def update_publisher_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.delete('/delete_publisher_nodes', versions=1)
+def delete_publisher_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.post('/create_affiliation_nodes', versions=1)
+def create_affiliation_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.delete('/delete_affiliation_nodes', versions=1)
+def delete_affiliation_nodes(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_affiliations', versions=1)
+def get_affiliations(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_affiliations_by_country', versions=1)
+def get_affiliations_by_country(values):
+	# Code goes here!
+	return ()
+
+@hug.get('/get_affiliations_by_domains', versions=1)
+def get_affiliations_by_domains(values):
+	# Code goes here!
+	return ()
