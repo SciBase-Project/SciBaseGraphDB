427e29ef3eb50cb9691d210ac025bb726a746708
cyberbeast <sandeshgade@gmail.com>
2016-02-26 11:50:25 +0530

    Implemented structural response handling for geophysical methods

--- modules/geophysical/geophysical_methods.py
+++ modules/geophysical/geophysical_methods.py
@@ -4,39 +4,46 @@ import hug
 def create_country_nodes(values):
 	'''Create country nodes to the GraphDB'''
 	# Code goes here!
-	return ("Acknowledged!")
+
+	return ("Created: " + str(values))
 
 @hug.post('/create_continent_nodes', versions=1)
 def create_continent_nodes(values):
 	# Code goes here! 
-	return ()
+	return ("Created: " + str(values))
 
 @hug.post('/attach_countries_to_continent', versions=1)
 def attach_countries_to_continent(values):
 	# Code goes here! 
-	return ()
+	output = []
+	for country, continent in values.items():
+		output.append("Attached {country} to {continent}".format(**locals()))
+	return (output)
 
 @hug.get('/get_countries', versions=1)
 def get_countries():
 	# Code goes here! 
-	return ()
+	return ("Here are all the countries!")
 
 @hug.get('/get_countries_by_continent', versions=1)
-def get_countries_by_continent():
+def get_countries_by_continent(continent_name):
+	print(continent_name[1:-1])
 	# Code goes here! 
-	return ()
+	output = {}
+	output[continent_name[1:-1]] = "Countries in this continent!"
+	return (output)
 
 @hug.get('/get_continents', versions=1)
 def get_continents():
 	# Code goes here! 
-	return ()
+	return ("Here are all the continents!")
 
 @hug.delete('/delete_country_nodes', versions=1)
 def delete_country_nodes():
 	# Code goes here! 
-	return ()
+	return ("All Countries Deleted!")
 
 @hug.delete('/delete_continent_nodes', versions=1)
 def delete_continent_nodes():
 	# Code goes here! 
-	return ()
+	return ("All Continents Deleted!")
