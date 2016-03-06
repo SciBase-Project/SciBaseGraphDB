0b1107ff50e9ab35fd7e3705f09d578c4697a536
cyberbeast <sandeshgade@gmail.com>
2016-03-06 13:04:16 +0530

    pre-lunch commit

---
+++ bin/pop_acm.py
@@ -0,0 +1,46 @@
+from py2neo import authenticate, Graph
+import json
+from pprint import pprint
+
+
+def create_country_nodes():
+	return
+
+def create_city_nodes():
+	return
+
+def create_continent_nodes():
+	return
+
+def create_journal_nodes():
+	return
+
+def create_article_nodes():
+	return
+
+def create_geophysical_nodes():
+	with open('config/geophysical.json') as geophysical_file:
+		geophysical = json.load(geophysical_file)
+
+	print("\t\t CONTINENTS:")
+	create_continent_nodes(list(geophysical.keys()))
+	print("\t\t COUNTRIES:")
+	create_country_nodes(list())
+	print("\t\t CITIES:")
+	create_city_nodes()
+	return
+
+def populate_db():
+	with open('config/neo4j_config.json') as config_file:
+		config = json.load(config_file)
+
+	authenticate(config['address'], config['username'], config['password'])
+	graph = Graph()
+	print("Connected to graph")
+
+	print("Building GEOPHYSICAL nodes in the SciBase Graph!")
+	create_geophysical_nodes()
+	return
+
+if __name__ == '__main__':
+	populate_db()
